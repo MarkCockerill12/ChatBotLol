80b0bc4f926c9b64b7bc7a0e1de6596b071de75d
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-09-25 14:46:27 +0100

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -10,6 +10,7 @@ prefix   = '`'
 server= ""
 port = 6667
 
+
 # Welcome!
 print("  ________  ___  ___  ________  _________        ________  ________  _________   ")
 print(" |\   ____\|\  \|\  \|\   __  \|\___   ___\     |\   __  \|\   __  \|\___   ___\ ")
@@ -19,6 +20,7 @@ print("   \ \  \____\ \  \ \  \ \  \ \  \   \ \  \       \ \  \|\  \ \  \\\  \  
 print("    \ \_______\ \__\ \__\ \__\ \__\   \ \__\       \ \_______\ \_______\   \ \__\ ")
 print("     \|_______|\|__|\|__|\|__|\|__|    \|__|        \|_______|\|_______|    \|__|")
 
+#User Inputs
 server= input("Please enter your desired server, if you don't enter anything it will default to '::1': ")
 if server == "":
     server = "::1"
@@ -33,12 +35,32 @@ nick= input("Please enter your desired nickname, if you don't enter anything it 
 if nick == (""):
     nick = "BotLol"
 
+channel = input("What channel should the bot join? If you don't enter anything it will default to #Test")
+if channel == "":
+    channel == "Test"
+
 #https://realpython.com/python-sockets/
 with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
     print(server)
     print(port)
     s.connect((server, port))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    
+    Connected = True
+    #Sends Nick and User to join server - Need to make it work with user inputs
+    s.send(b"NICK BotLol\r\n")
+    s.send(b"USER Bot Bot2 Bot3 Bot4\r\n")
 
-print(f"Received {data!r}")
+    #Commands for Bot to do - Need to make it work with user inputs
+    s.send(b"JOIN #Test\r\n")
+    s.send (b"PRIVMSG #Test :Hello World.\r\n")
+    
+    #Handler for Ping / Pong requests to avoid timeout needs work
+    #while Connected
+     #data = s.recv(1024).decode
+    # print(data)
+     #if data[0:4] == "PING"
+     # print(data)
+    #  s.send(b"PONG" + data.split()[1]+"r\n")
+     # print("Pong")
+
+    
