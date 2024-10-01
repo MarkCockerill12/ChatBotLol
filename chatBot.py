9fbfca23835bcc9d2a200b2aca91d42a0527c058
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2024-10-01 13:24:25 +0100

    Classes and methods functional..?

--- chatBot.py
+++ chatBot.py
@@ -1,17 +1,18 @@
 import socket
 import time
 
-#Badly adding classes and objects WORK IN PROGRESS !!
+#Added classes and method structure for the bot, definetly needs improving though.
 
 class Bot: 
 
     def __init__(self, server="::1", port=6667, nick="BotLol", channel="#Test"):
-    # Variables
+    # Making an instance of bot class 
         self.server = server
         self.port = port
         self.nick = nick
         self.channel = channel
         self.channel_info = {}
+        self.Connected = False
     
 
     # Welcome!
@@ -33,17 +34,18 @@ class Bot:
                 self.s.send(f"JOIN {self.channel}\r\n".encode('utf-8'))
 
                 # Wait for server's response before sending a message
-                Connected = Truee
+                self.Connected = True
                 self.running_bot()
 
         except Exception as e:
-        print(f"Socket error occurred: {e}")
+            print(f"Socket error occurred: {e}")
 
-
+  
     def running_bot(self):
         
+        
         while self.Connected:
-            data = s.recv(1024).decode('utf-8')
+            data = self.s.recv(1024).decode('utf-8')
             if not data:
                 print("Disconnected from server.")
                 break
@@ -51,11 +53,11 @@ class Bot:
             print("Server Response:", data.strip())  # Debugging line to check server's response
 
             # Handle channel join confirmation
-            if f":{nick}!{nick}@".lower() in data.lower() and "join".lower() in data.lower():
-                print(f"Successfully joined {channel}. Sending message...")
-                s.send(f"PRIVMSG {channel} :Hello {channel}. I am {nick}, the friendly chat Bot, say !help to find out how to talk to me.\r\n".encode('utf-8'))
-                print(f"Message sent to {channel}: 'Hello {channel}.'")
-                channel_info[channel] = {
+            if f":{self.nick}!{self.nick}@".lower() in data.lower() and "join".lower() in data.lower():
+                print(f"Successfully joined {self.channel}. Sending message...")
+                self.s.send(f"PRIVMSG {self.channel} :Hello {self.channel}. I am {self.nick}, the friendly chat Bot, say !help to find out how to talk to me.\r\n".encode('utf-8'))
+                print(f"Message sent to {self.channel}: 'Hello {self.channel}.'")
+                self.channel_info[self.channel] = {
                     'user': self.nick,
                     'channel': self.channel,
                     'timestamp': time.time(),
@@ -63,7 +65,7 @@ class Bot:
                 }
                 print(f"Stored channel information: {self.channel_info[self.channel]}")
 
-            # Check for PING message from server
+            # Check for PING message from server - Needs updating
             if data.startswith("PING"):
                 ping_response = data.split()[1]
                 self.s.send(f"PONG {ping_response}\r\n".encode('utf-8'))
@@ -76,6 +78,8 @@ class Bot:
                 
                 
     def handle_privmsg(self,data):
+        #Handling for messages 
+
         sender = data.split('!', 1)[0][1:]
         message = data.split('PRIVMSG', 1)[1].split(':', 1)[1]
 
@@ -83,26 +87,26 @@ class Bot:
 
         if message.strip().lower() == "!quit":
             response = f"Alright, see ya, {sender}!"
-            self.s.send(f"PRIVMSG {channel} :{response}\r\n".encode('utf-8'))
-            Connected = False
+            self.s.send(f"PRIVMSG {self.channel} :{response}\r\n".encode('utf-8'))
+            self.Connected = False
             print("Quitting...")
 
         elif message.strip().lower() == "!hello":
             response = f"Hello, {sender}!"
-            self.s.send(f"PRIVMSG {channel} :{response}\r\n".encode('utf-8'))
+            self.s.send(f"PRIVMSG {self.channel} :{response}\r\n".encode('utf-8'))
             print(f"Responded to {sender}: {response}")
 
         elif message.strip().lower() == "!help":
             response = "!hello - I will say hello back to you. !quit - I will say goodbye and leave the channel. !savedata - I will show you the stored channel information."
-            self.s.send(f"PRIVMSG {channel} :{response}\r\n".encode('utf-8'))
+            self.s.send(f"PRIVMSG {self.channel} :{response}\r\n".encode('utf-8'))
             print(f"Responded to {sender}: {response}")
 
         elif message.strip().lower() == "!savedata":
-            if channel in channel_info:
-            response = f"Stored channel information: {channel_info[channel]}"
-            self.s.send(f"PRIVMSG {channel} :{response}\r\n".encode('utf-8'))
-            print(f"Responded to {sender}: {response}")
-        
+            if self.channel in self.channel_info:
+                response = f"Stored channel information: {self.channel_info[channel]}"
+                self.s.send(f"PRIVMSG {self.channel} :{response}\r\n".encode('utf-8'))
+                print(f"Responded to {sender}: {response}")
+            
             else:
                 response = "No channel information saved yet."
                 self.s.send(f"PRIVMSG {channel} :{response}\r\n".encode('utf-8'))
@@ -116,7 +120,7 @@ class Bot:
     
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
 
         # User Inputs
         server = input("Please enter your desired server, if you don't enter anything it will default to '::1': ") or "::1"
@@ -131,7 +135,7 @@ class Bot:
         nick = input("Please enter your desired nickname, if you don't enter anything it will default to BotLol: ") or "BotLol"
         channel = input("What channel should the bot join? If you don't enter anything it will default to #Test: ") or "#Test"
 
-        runbot= bot(server,port,nick,channel)
+        runbot= Bot(server,port,nick,channel)
         
         try:
             runbot.connect()
@@ -142,7 +146,7 @@ class Bot:
     
 
 
-    # Exception handling
+    # Exception handling- needs to be added back in
         #except KeyboardInterrupt:
         # print("Bot interrupted, closing connection...")
         #except socket.error as e:
